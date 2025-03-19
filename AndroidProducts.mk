f7d3da2e251c6a469833e0f998f66e4cf8a6c3f4
Meisie Widuri <dotkit@electrowizard.me>
2025-03-20 05:29:37 +0700

    X00TD: Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X00TD.mk
+    $(LOCAL_DIR)/alpha_X00TD.mk
+
+COMMON_LUNCH_CHOICES := \
+    alpha_X00TD-user \
+    alpha_X00TD-userdebug \
+    alpha_X00TD-eng
