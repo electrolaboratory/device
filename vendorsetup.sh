c237a3a68c6682dda7c54e4295da0013a81d3bfc
texascake <tiktodz@soulvibe-team.cloud>
2026-05-26 23:35:59 +0700

    X00TD: Regenerate
    Comment out the signing process in vendorsetup.sh.
    axion has sign key by default with gk -s

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,9 +11,9 @@ rm -rf hardware/qcom-caf/sdm660/audio
 rm -rf hardware/qcom-caf/sdm660/display
 
 ## Cloning
-git clone --depth=1 https://github.com/sotodrom/device_common -b inf device/asus/sdm660-common
-git clone --depth=1 https://github.com/sotodrom/kernel_qcom_sdm660 -b newcam --single-branch kernel/asus/sdm660
-git clone --depth=1 https://github.com/sotodrom/vendor_asus vendor/asus
+git clone --depth=1 https://github.com/electrolaboratory/common -b ax device/asus/sdm660-common
+git clone --depth=1 https://github.com/rsuntk/android_kernel_asus_sdm660-4.19 -b lineage-23.2 --single-branch kernel/asus/sdm660
+git clone --depth=1 https://github.com/electrolaboratory/vendor_asus -b 16 vendor/asus
 
 git clone https://github.com/Kyura-Ground/android_hardware_qcom_audio hardware/qcom-caf/sdm660/audio
 git clone https://github.com/Kyura-Ground/android_hardware_qcom_display hardware/qcom-caf/sdm660/display
@@ -21,14 +21,5 @@ git clone https://github.com/Kyura-Ground/android_hardware_lineage_interfaces ha
 
 ## Integrate KernelSU
 cd kernel/asus/sdm660
-curl -LSs "https://raw.githubusercontent.com/Sorayukii/KernelSU-Next/stable/kernel/setup.sh" | bash -s hookless
-cd -
-
-#### signing
-rm -rf vendor/lineage-priv/keys
-mkdir -p vendor/lineage-priv/keys
-sed -i 's|PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/testkey|PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey|g' lineage/scripts/lineage-priv-template/keys.mk
-cp -R lineage/scripts/lineage-priv-template/* vendor/lineage-priv/keys/
-cd vendor/lineage-priv/keys
-bash keys.sh
+curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s staging/xxksu
 cd -
