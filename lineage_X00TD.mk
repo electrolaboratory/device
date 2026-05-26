d6224b197a5e865892362373721ffda8d2493fd6
Tiktodz <dotkit@electrowizard.me>
2026-05-26 13:00:12 +0000

    X00TD: For LunarisAOSP

--- lineage_X00TD.mk
+++ lineage_X00TD.mk
@@ -8,26 +8,22 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Infinity-X stuff.
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Infinity-X Specific Flags
-INFINITY_MAINTAINER := EunjiX
-INFINITY_BUILD_TYPE := UNOFFICIAL
-TARGET_HAS_UDFPS := false
+# Specific Flags
+SURFACE_FLINGER_BOOST := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_BOOT_ANIMATION_RES := 1080
-
-ifeq ($(WITH_GAPPS), true)
-TARGET_BUILD_GOOGLE_TELEPHONY := true
-TARGET_EXCLUDES_VIA := true
-endif
+TARGET_SUPPORTS_GOOGLE_TELEPHONY := false
+WITH_BCR := false
+WITH_GMS := false
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := infinity_X00TD
+PRODUCT_NAME := lineage_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,13 +6,18 @@ export TZ=Asia/Jakarta
 rm -rf device/asus/sdm660-common
 rm -rf kernel/asus
 rm -rf vendor/asus
-#rm -rf lineage/scripts
+rm -rf hardware/lineage/interfaces
+rm -rf hardware/qcom-caf/sdm660/audio
+rm -rf hardware/qcom-caf/sdm660/display
 
 ## Cloning
 git clone --depth=1 https://github.com/sotodrom/device_common -b inf device/asus/sdm660-common
 git clone --depth=1 https://github.com/sotodrom/kernel_qcom_sdm660 -b newcam --single-branch kernel/asus/sdm660
-git clone --depth=1 https://github.com/sotodrom/vendor_asus -b np vendor/asus
-#git clone --depth=1 https://github.com/LineageOS/scripts lineage/scripts
+git clone --depth=1 https://github.com/sotodrom/vendor_asus vendor/asus
+
+git clone https://github.com/Kyura-Ground/android_hardware_qcom_audio hardware/qcom-caf/sdm660/audio
+git clone https://github.com/Kyura-Ground/android_hardware_qcom_display hardware/qcom-caf/sdm660/display
+git clone https://github.com/Kyura-Ground/android_hardware_lineage_interfaces hardware/lineage/interfaces
 
 ## Integrate KernelSU
 cd kernel/asus/sdm660
@@ -20,10 +25,10 @@ curl -LSs "https://raw.githubusercontent.com/Sorayukii/KernelSU-Next/stable/kern
 cd -
 
 #### signing
-rm -rf vendor/infinity-priv/keys
-mkdir -p vendor/infinity-priv/keys
-sed -i 's|PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/testkey|PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/infinity-priv/keys/releasekey|g' lineage/scripts/lineage-priv-template/keys.mk
-cp -R lineage/scripts/lineage-priv-template/* vendor/infinity-priv/keys/
-cd vendor/infinity-priv/keys
+rm -rf vendor/lineage-priv/keys
+mkdir -p vendor/lineage-priv/keys
+sed -i 's|PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/testkey|PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey|g' lineage/scripts/lineage-priv-template/keys.mk
+cp -R lineage/scripts/lineage-priv-template/* vendor/lineage-priv/keys/
+cd vendor/lineage-priv/keys
 bash keys.sh
 cd -
